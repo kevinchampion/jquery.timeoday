6c38bf25d8b6a8d63c6c48b0044f7ced2b412e17
Cory Kaufman-Schofield <cory@corykaufman.com>
2013-04-18 21:15:31 -0400

    Made key up/down work

--- jquery.timeoday.js
+++ jquery.timeoday.js
@@ -11,9 +11,9 @@
 
     var $input = $(spec.input),
       options = $.extend({}, defaults, spec.options),
-      $results = $('<ol></ol>').addClass(options.resultsClass),
+      $results = $('<ol><li>1</li><li>2</li><li>3</li></ol>').addClass(options.resultsClass),
       activeClassSelector = '.' + options.activeClass.split(' ').join('.'),
-      focused, visible, mouseover;
+      visible, mouseover;
 
     var init = function() {
       $input.on('focus', focus);
@@ -28,35 +28,110 @@
     };
 
     var focus = function() {
-      console.log('Focus');
+      show();
     };
 
     var blur = function() {
-      console.log('Blur');
+      if (!mouseover) { hide() }
     }
 
     var keypress = function() {
       console.log('Key press');
     };
 
-    var keyup = function() {
-      console.log('Key up');
+    var keyup = function(e) {
+      if (~$.inArray(e.keyCode, [16, 17, 18, 38, 40])) {
+        return;
+      }
+
+      if (~$.inArray(e.keyCode, [9, 13])) {
+        if (visible) {
+          select();
+        }
+        return;
+      }
+
+      if (e.keyCode == 27) {
+        if (visible) {
+          hide();
+        }
+        return;
+      }
+
+      render();
+
+      return false;
     };
 
-    var keydown = function() {
-      console.log('Key down');
+    var keydown = function(e) {
+      move(e);
+    };
+
+    var move = function(e) {
+      if (!visible) { return }
+      
+      // Ignore tab, enter or escape
+      if (~$.inArray(e.keyCode, [9, 13, 27])) {
+        e.preventDefault();
+        return;
+      }
+
+      // Up arrow
+      if (e.keyCode == 38) {
+        e.preventDefault();
+        previous();
+        return;
+      }
+
+      // Down arrow
+      if (e.keyCode == 40) {
+        e.preventDefault();
+        next();
+        return;
+      }
+      
+      e.stopPropagation();
+    };
+
+    var previous = function() {
+      var $current = $results.children(activeClassSelector),
+        $previous = $current.prev();
+      
+      if ($previous.length) {
+        $current.removeClass(options.activeClass);
+        $previous.addClass(options.activeClass);
+      }
+    };
+
+    var next = function() {
+      var $current = $results.children(activeClassSelector),
+        $next = $current.next();
+
+      if ($next.length) {
+        $current.removeClass(options.activeClass);
+        $next.addClass(options.activeClass);
+      }
     };
 
     var click = function() {
       console.log('Click');
     };
 
-    var mouseenter = function() {
+    var mouseenter = function(e) {
       console.log('Mouse Enter');
     };
 
     var mouseleave = function() {
       console.log('Mouse Leave');
+    };
+
+    var hide = function() {
+    };
+
+    var show = function() {
+      $results.children(':first').addClass(options.activeClass);
+      $results.insertAfter($input).show();
+      visible = true;
     };
 
     init();
